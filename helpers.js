d0163b495f0ef0c9ae32f000ae6015b2a6d93c33
Fredrik Sundberg <fredrik@sundbergmedia.com>
2018-03-05 20:19:07 +0100

    Cleanup

--- helpers.js
+++ helpers.js
@@ -21,13 +21,8 @@ function returnJson(url) {
   });
 }
 
-// helper: if string is valid JSON
 function isJson(str) {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
-  }
+  try { JSON.parse(str); } catch (e) { return false; }
   return true;
 }
 
@@ -38,8 +33,6 @@ function getIpData(ip) {
     if(!ip || ip == undefined) return resolve('(no data)');
     if(isLocalIp(ip)) ip = process.env.EXTERNAL_IP; // use local external IP if local IP
 
-    // TODO: this needs to be wrapped in a separated helper...
-    // resolveHostName // ?
     return dnsLookup(ip).then(function(ip) {
       return getCoords(ip);
     })
@@ -67,7 +60,6 @@ function prepareIpData(data) {
     let diffCoords = data.extra.latitude + ',' + data.extra.longitude;
     data.mapZoomedAlt = getGoogleMap(diffCoords, 13);
   }
-
   return data;
 }
 
@@ -87,7 +79,6 @@ function getGoogleMap(coordinates, zoom, size) {
 // get some geolocation data from coordinates using Gogle Maps API
 function getCoordData(coordinates) {
   let url = MAPS_API_URL + "geocode/json?address=" + coordinates;
-  log('>> URL', url);
   return new Promise(function(resolve, reject) {
     return request.get(url, function(err, response, body) {
       let json = (isJson(body) ? JSON.parse(body) : body);
@@ -111,7 +102,7 @@ function dnsLookup(url, callback) {
     if(!isDomain(url)) return resolve(url); // avoid lookup when handling IP addresses
     return dns.lookup(url, function(err, result) {
       if(err) reject(err);
-      console.log(result);
+      log(result);
       resolve(result)
     });
   })
@@ -130,25 +121,13 @@ function isLocalIp(ip) {
   return (ip.indexOf('127.0.0.1') > -1 || ip.indexOf('::1') > -1 || ip.indexOf('localhost') > -1);
 }
 
-// contains at least 2 alpha chars and a .
 function isDomain(name) {
-  return (name.match(/[a-z]/i));
+  return (name.match(/[a-z]/i)); // contains at least 2 alpha chars and a .
 }
 
 function getExternalIp() {
   return returnJson(EXTERNAL_IP_URL);
 }
-
-
-// get external IP on init
-// (function() {
-//   console.log('INIT');
-//   getExternalIp().then(function(ip) {
-//     console.log('External IP:', ip);
-//     localIp = ip; // set local external IP
-//     // process.exit(); // kill a kill
-//   })
-// })();
 
 
 module.exports = {
