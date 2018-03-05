30710fd99679d3e16139ddee50377aa8d8538c36
Fredrik Sundberg <fredrik@sundbergmedia.com>
2018-03-05 19:43:09 +0100

    Cleanup & refactorization

--- server.js
+++ server.js
@@ -14,18 +14,9 @@ var helpers = require('./helpers');
 var request = require("request");
 var express = require('express');
 var Promise = require('bluebird');
-var bunyan = require('bunyan');
-var chalk = require('chalk');
-var logger = bunyan.createLogger({name: "drink-routes"});
-var log = console.log; // TODO: logger.info() w/ concat arguments
-// log('ENVIRONMENT VARS:', process.env);
-
-
-// temp configs
-var localIp = '90.227.59.52';
-// var coords = '57.6667,14.95';    // Eksjö
-var coords = '57.614574,15.584994'; // Mariannelund
 var app = express();
+var log = console.log;
+
 
 // app config
 // app.use(express.static(__dirname)); // danger!
@@ -36,27 +27,23 @@ app.use('/data', require('./routes.js'));
 app.locals.pretty = true;
 
 
-// get external IP on init
-// (function() {
-//   console.log('INIT');
-//   helpers.getExternalIp().then(function(ip) {
-//     console.log('External IP:', ip);
-//     localIp = ip; // set local external IP
-//     // process.exit(); // kill a kill
-//   })
-// })();
-
+function getIp(req) {
+  return req.params.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip;
+}
 
 // helper; get data for IP
+// TODO: moved to helpers.getAndParseIpData
 function getIpData(ip) {
   log('Get data for IP:', ip);
   return new Promise(function(resolve, reject) {
     if(!ip || ip == undefined) return resolve('(no data)');
-    if(helpers.isLocalIp(ip)) ip = localIp; // use local external IP if local IP
+    if(helpers.isLocalIp(ip)) ip = process.env.EXTERNAL_IP; // use local external IP if local IP
 
     // TODO: this needs to be wrapped in a separated helper...
-    helpers.dnsLookup(ip)
-    .then(helpers.getCoords())
+    helpers.dnsLookup(ip).then(function(ip) {
+      return helpers.getCoords(ip);
+    })
+    // .then(helpers.getCoords())
     .then(function(data) {
       log('COORDS dataa:', data);
       let extra = data;
@@ -65,14 +52,7 @@ function getIpData(ip) {
         data.ip = ip;
         data.extra = extra;
         data.map = helpers.getGoogleMap(coords);
-        data.mapZoomed = helpers.getGoogleMap(coords, 13); // extra zoom
-        
-        if(data.extra.latitude) {
-          let diffCoords = data.extra.latitude + ',' + data.extra.longitude;
-          data.mapZoomedAlt = helpers.getGoogleMap(diffCoords, 13);
-        } else {
-          data.mapZoomedAlt = false;
-        }
+        data.mapZoomed = helpers.getGoogleMap(coords, 13); // extra zoom        
         return data;
         // resolve(data);
       })
@@ -82,43 +62,20 @@ function getIpData(ip) {
   });
 }
 
-// helper: revolve domain name to IP adress by DNS lookup
-function resolveHostName(address) {
-  return new Promise(function(resolve, reject) {
-    // reject if missing address
-    if(!address || address == undefined) return reject('(no data)');
-
-    if(address.match(/[a-z]/i)) {
-      // try resolve by DNS lookup
-      // TODO: validate URL?
-      resolve(helpers.dnsLookup(address));
-    } else {
-      // TODO: validate IP address
-      resolve(address);
-    }
-
-  }); 
-}
-
 // index page; get data for the client's IP address
 app.get('/', function (req, res) {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip;  
-  getIpData(ip).then(function(data) {
+  getIpData(getIp(req)).then(function(data) {
     log('DATA:', data);
-    res.json(data);
+    res.render('template', { herp: 'Geolocation ting', data: data }) // render HTML template
+    // res.json(data);
   });
-
 });
 
 // get options for external IP
 app.get('/ip/:ip', function (req, res) {
-  var ip = req.params.ip;
-  log(chalk.red('GETTING INFO FOR ADDRESS:' + ip));
-  log(chalk.blue('Hello') + ' World' + chalk.red('!'));
-
-  getIpData(ip).then(function(data) {
+  getIpData(getIp(req)).then(function(data) {
     data.input = req.params.ip;
-    log('DATA:', data);
+    log('DATA:', data); // log output data
     res.render('template', { herp: 'Stats for ' + ip, data: data }) // render HTML template
   });
 
@@ -128,7 +85,6 @@ app.get('/ip/:ip', function (req, res) {
 // TODO: validate domain name
 app.get('/whois/:domain', function (req, res) {  
   helpers.whoisLookup(req.params.domain).then(function(data) {
-    log('WHOIS data:', data);
     res.send('<pre>' + data + '</pre>');
   })
 });
