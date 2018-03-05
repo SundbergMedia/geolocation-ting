f6dac13fac8efcfbe8237faa72248d96ac439b70
Fredrik Sundberg <fredrik@sundbergmedia.com>
2018-03-05 19:53:32 +0100

    Cleanup & fixes

--- helpers.js
+++ helpers.js
@@ -4,18 +4,26 @@ var config = require('./config');
 var coords = config.coords;
 var request = require('request');
 var dns = require('dns');
-var whois = require('whois')
+var whois = require('whois');
+var log = console.log;
+require('dotenv').config();
 
+const MAPS_API_URL = 'https://maps.googleapis.com/maps/api/';
+const GEOLOCATION_API_URL = 'http://freegeoip.net/json/';
+const EXTERNAL_IP_URL = 'http://icanhazip.com';
+const EXTERNAL_IP = process.env.EXTERNAL_IP;
 
 // helper: HTTP GET request -> JSON object or HTML string
 function returnJson(url) {
+  log('>> URL', url);
   return new Promise(function(resolve, reject) {
     return request.get(url, function(err, response, body) {
-      resolve((isJson(body) ? JSON.parse(body) : body));
+      return resolve((isJson(body) ? JSON.parse(body) : body));
     });
   });
 }
 
+// helper: if string is valid JSON
 function isJson(str) {
   try {
     JSON.parse(str);
@@ -25,26 +33,67 @@ function isJson(str) {
   return true;
 }
 
+// helper; get data for IP
+function getIpData(ip) {
+  log('Get data for IP:', ip);
+  return new Promise(function(resolve, reject) {
+    if(!ip || ip == undefined) return resolve('(no data)');
+    if(isLocalIp(ip)) ip = process.env.EXTERNAL_IP; // use local external IP if local IP
+
+    // TODO: this needs to be wrapped in a separated helper...
+    // resolveHostName // ?
+    return dnsLookup(ip).then(function(ip) {
+      return getCoords(ip);
+    })
+    .then(function(data) {
+      log('COORDS:', data);
+      let extra = data;
+      let coords = data.latitude + ',' + data.longitude;
+      // TODO: support for returnig coords only, since google quota is limited...
+      getCoordData(coords).then(function(data) {
+        data.ip = ip;
+        data.extra = extra;
+        return resolve(prepareIpData(data));
+      })
+    })
+  });
+}
+
+// helper: prepare parsed IP data for output
+function prepareIpData(data) {
+  data.map = getGoogleMap(coords);
+  data.mapZoomed = getGoogleMap(coords, 13); // extra zoom
+  data.mapZoomedAlt = false;
+  
+  if(data.extra.latitude) {
+    let diffCoords = data.extra.latitude + ',' + data.extra.longitude;
+    data.mapZoomedAlt = getGoogleMap(diffCoords, 13);
+  }
+
+  return data;
+}
+
 function getCoords(ip) {
-  let url = 'http://freegeoip.net/json/' + ip;
-  return returnJson(url);
+  return returnJson(GEOLOCATION_API_URL + ip);
 }
 
 // return URL to google maps for coordinates
+// TODO: cache maps locally in order to avoid overusing dat daily quata
 function getGoogleMap(coordinates, zoom, size) {
   if(!zoom) zoom = 10;
   if(!size) size = '400x400';
-  let url = 'https://maps.googleapis.com/maps/api/staticmap?center=' + coordinates + '&zoom=' + zoom + '&size=' + size + '&markers&markers=color:blue%7Clabel:S%7C' + coordinates + '&key=' + config.apiKey;
+  let url = MAPS_API_URL + 'staticmap?center=' + coordinates + '&zoom=' + zoom + '&size=' + size + '&markers&markers=color:blue%7Clabel:S%7C' + coordinates + '&key=' + config.apiKey;
   return url;
 }
 
 // get some geolocation data from coordinates using Gogle Maps API
 function getCoordData(coordinates) {
-  let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + coordinates;
-
+  let url = MAPS_API_URL + "geocode/json?address=" + coordinates;
+  log('>> URL', url);
   return new Promise(function(resolve, reject) {
     return request.get(url, function(err, response, body) {
-      let json = JSON.parse(body);
+      let json = (isJson(body) ? JSON.parse(body) : body);
+      if(!json || json == undefined || !json.results || !json.results[0]) return resolve('unparsable data: ' + body);
       let row = json.results[0];
       if(!row) return resolve(false);
       let output = {
@@ -61,21 +110,36 @@ function getCoordData(coordinates) {
 // TODO: validate URL, etc...
 function dnsLookup(url, callback) {
   return new Promise(function(resolve, reject) {
-    // TODO: reject if cannot find host
     return dns.lookup(url, function(err, result) {
+      if(err) reject(err);
       console.log(result);
       resolve(result)
     });
   })
-
 }
 
 function whoisLookup(url) {
   return new Promise(function(resolve, reject) {
     return whois.lookup(url, function(err, data) {
+      if(err) reject(err);
       resolve(data);
     });
   });
+}
+
+function resolveHostName(address) {
+  return new Promise(function(resolve, reject) {
+    if(!address || address == undefined) return reject('(no data)'); // reject if missing address
+
+    if(address.match(/[a-z]/i)) {
+      // try resolve by DNS lookup
+      // TODO: validate URL?
+      resolve(dnsLookup(address));
+    } else {
+      // TODO: validate IP address
+      resolve(address);
+    }
+  }); 
 }
 
 function isLocalIp(ip) {
@@ -83,10 +147,21 @@ function isLocalIp(ip) {
 }
 
 function getExternalIp() {
-  return returnJson('http://icanhazip.com').then(function(ip) {
+  return returnJson(EXTERNAL_IP_URL).then(function(ip) {
     return Promise.resolve(ip);
   })
 }
+
+
+// get external IP on init
+// (function() {
+//   console.log('INIT');
+//   getExternalIp().then(function(ip) {
+//     console.log('External IP:', ip);
+//     localIp = ip; // set local external IP
+//     // process.exit(); // kill a kill
+//   })
+// })();
 
 
 module.exports = {
@@ -97,4 +172,6 @@ module.exports = {
   whoisLookup, 
   isLocalIp, 
   getExternalIp,
+  getIpData,
+  resolveHostName,
 }
