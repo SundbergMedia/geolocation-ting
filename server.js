0ff1bf5607b442bade59aa48fc975fe6cd3a44ab
Fredrik Sundberg <fredrik@sundbergmedia.com>
2018-03-12 15:25:28 +0100

    Refactored sendResult into common function

--- server.js
+++ server.js
@@ -30,11 +30,17 @@ function getIp(req) {
   return req.params.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip;
 }
 
+function sendResult(data, res, title) {
+  if(!title) title = 'Geolocation ting';
+  log('DATA:', data);
+  res.render('template', { title: title, data: data }) // render HTML template
+}
+
+
 // index page; get data for the client's IP address
 app.get('/', function (req, res) {
   helpers.getIpData(getIp(req)).then(function(data) {
-    log('DATA:', data);
-    res.render('template', { herp: 'Geolocation ting', data: data }) // render HTML template
+    sendResult(data, res);
   });
 });
 
@@ -43,8 +49,7 @@ app.get('/ip/:ip', function (req, res) {
   var ip = getIp(req);
   helpers.getIpData(ip).then(function(data) {
     data.input = ip;
-    log('DATA:', data);
-    res.render('template', { herp: 'Stats for ' + ip, data: data }) // render HTML template
+    sendResult(data, res, 'Geolocation ting - ' + ip);
   });
 });
 
@@ -53,7 +58,7 @@ app.get('/ip/:ip', function (req, res) {
 app.get('/whois/:domain', function (req, res) {  
   helpers.whoisLookup(req.params.domain).then(function(data) {
     res.send('<pre>' + data + '</pre>');
-  })
+  });
 });
 
 // spin up the server
